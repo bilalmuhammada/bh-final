80c7d8c20ae5c22af0072950f2a9ac0345ec920b
bilalmuhammada <bilalahmada774@gmail.com>
2024-10-16 22:23:54 +0200

    Update image&file.blade.php

--- resources/views/listings/image&file.blade.php
+++ resources/views/listings/image&file.blade.php
@@ -9,12 +9,12 @@
         <span><b>Add Photos</b></span>
     </div>
 </div>
-<div class="col-md-4 mx-auto" style="margin-top:30px; margin-left: 24pc !important; max-width: 53% !important;">
+<div class="col-md-4" style="margin-top:30px; margin-left: 24pc !important; max-width: 53% !important;">
 
     <div id="image-display-div" style="display: flex; flex-wrap: wrap;"></div>
 </div>
 
-<div class="col-md-6 mx-auto" style="margin-top: 20px;">
+<div class="col-md-6 mx-auto">
     <div class="input--file">
         <i class="fa fa-camera fa-1x"></i>
         <input type="file" multiple class="form-controlz form-control-file documents"  id="fileInput" name="documents[]"
