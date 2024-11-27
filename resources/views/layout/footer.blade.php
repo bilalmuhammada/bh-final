f6be695392c3448ff3a69ba2ef8d6cd7a2d69c18
bilalmuhammada <bilalahmada774@gmail.com>
2024-11-27 17:34:53 +0100

    push

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -21,9 +21,9 @@
 
    @keyframes shake {
     0% { transform: translateX(0); }
-    25% { transform: translateX(-2px); }
-    50% { transform: translateX(2px); }
-    75% { transform: translateX(-2px); }
+    25% { transform: translateX(-5px); }
+    50% { transform: translateX(5px); }
+    75% { transform: translateX(-5px); }
     100% { transform: translateX(0); }
   }
 </style>
