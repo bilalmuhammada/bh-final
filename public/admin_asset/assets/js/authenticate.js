583ad6dbd758a419d1db3327dfd11507648fa9ad
bilalmuhammada <bilalahmada774@gmail.com>
2025-08-14 16:33:24 +0200

    o

--- public/admin_asset/assets/js/authenticate.js
+++ public/admin_asset/assets/js/authenticate.js
@@ -37,7 +37,7 @@ $(document).on('click', '.register-button', function () {
 
 //submitting login form
 $(document).on('click', '.login-submit-button', function () {
-   
+    alert("ddd");
     $.ajax({
         url: api_url + 'login',
         data: $('.login-form').serialize(),
