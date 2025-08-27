ea3d74f424341d2e3eb93163f6b1830e6c3418b0
bilalmuhammada <bilalahmada774@gmail.com>
2025-08-27 16:56:30 +0200

    Update list.blade.php

--- resources/views/chats/list.blade.php
+++ resources/views/chats/list.blade.php
@@ -323,14 +323,14 @@ a:hover {
                                
                                 <div class="col-md-2 hiddentrash">
                                     <div class="row">
-                                        <div class="col-md-12 text-center" style="margin: 9px 0px 0px 14rem;">
+                                        <div class="col-md-12 text-center" style="margin: 9px 0px 0px 12.6rem;">
                                             <i class="fa fa-trash" style="color: rgb(9, 9, 166);"></i>
                                         </div>
                                     </div>
                                 </div>
                                 <div class="col-md-2 edit">
                                     <div class="row">
-                                        <div class="col-md-12 text-center" style="margin: 9px 0px 0px 14rem;">
+                                        <div class="col-md-12 text-center" style="margin: 9px 0px 0px 12.6rem;">
                                             <i class="fa fa-pencil" id="edit-icon" style="color: rgb(9, 9, 166);"></i>
                                         </div>
                                     </div>
