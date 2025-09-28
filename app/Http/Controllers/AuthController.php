052f0a72c56178c6ebaa7b695c599133404479bf
bilalmuhammada <bilalahmada774@gmail.com>
2025-09-28 11:49:53 +0300

    Update AuthController.php

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -27,7 +27,7 @@ class AuthController extends Controller
     public function index()
     {
       
-        dd(Session::all());
+        
         if (Session::has('user')) {
            
             return redirect('/dashboard');
