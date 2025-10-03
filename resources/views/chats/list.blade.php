9107308b738533df4dadf2678090f8f2cb1716f0
bilalmuhammada <bilalahmada774@gmail.com>
2025-10-03 20:45:07 +0300

    Update list.blade.php

--- resources/views/chats/list.blade.php
+++ resources/views/chats/list.blade.php
@@ -547,7 +547,7 @@ a:hover {
                                                 <!-- Bottom: Time & Unread badge -->
                                                 <div style="display: flex; align-items: center; gap: 6px; font-size: 12px; color: #666;">
                                                     <div class="badge bgg-yellow badge-pill unread-count" 
-                                                        style="display: {{ ($login_user_id != $chat->latest_message_sender_id && $chat->unread_count > 0) ? 'block' : 'none' }};">
+                                                        style="display: {{ ($login_user_id != $chat->latest_message_sender_id && $chat->unread_count > 0) ? 'block' : 'none' }};border-radius: 30px;">
                                                         {{ $chat->unread_count }}
                                                     </div>
 
