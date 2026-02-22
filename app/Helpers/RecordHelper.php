051ab8181ddcc82def10aaba648fd52ecfd52c48
bilalmuhammada <bilalahmada774@gmail.com>
2026-02-22 21:50:39 +0100

    Update RecordHelper.php

--- app/Helpers/RecordHelper.php
+++ app/Helpers/RecordHelper.php
@@ -174,7 +174,7 @@ class RecordHelper
  
         $table = $category->form_view;
 
-        return Listing::with(['attachments'])->select('listings.*', 'details.title', 'details.price', 'details.location_name')
+        return Listing::with(['attachments'])->select('listings.*')
             ->join($table . ' as details', 'details.listing_id', '=', 'listings.id')
             ->where('listings.category_id', $category_id)
             ->when($country_id, function ($q) use ($country_id) {
